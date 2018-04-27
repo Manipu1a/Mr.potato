d1acfe5a8704fdb2a276d7797327c2355646b0f3
manipula <1912321843@qq.com>
2018-04-27 18:17:27 +0800

    second commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [HideInInspector]
+    public bool facingRight = true;         // For determining which way the player is currently facing.
+    [HideInInspector]
+    public bool jump = false;
+
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+
+    private Transform groundCheck;          // A position marking where to check if the player is grounded.
+    private bool grounded = false;
+    private Animator anim;
+
+    void Awake()
+    {
+        groundCheck = transform.Find("groundCheck");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+   void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+        }
+        if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+
+            jump = false;
+        }
+        
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
